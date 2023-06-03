d962d8d9241f751fd5929b1e9059ffa07ca29bed
Rostislav <rostik057@gmail.com>
2023-06-03 23:21:34 +0300

    +add input tags

--- src/ui/InputMulti/InputMulti.tsx
+++ src/ui/InputMulti/InputMulti.tsx
@@ -1,22 +1,64 @@
-import React, { FC, InputHTMLAttributes } from 'react';
+import React, { FC, InputHTMLAttributes, useState } from 'react';
 import styles from './InputMulti.module.scss';
 import InputTag from '../InputTag/InputTag';
 
 interface InputMultiProps extends InputHTMLAttributes<HTMLInputElement> { }
 
+type tag = Array<{
+  id: string,
+  value: string
+}>;
+
 const InputMulti: FC<InputMultiProps> = props => {
   const { className, ...rest } = props;
   const styleClassName = className ? ' ' + className : '';
 
+  const [value, setValue] = useState(props.value ?? '')
+
+  const [tags, setTags] = useState<tag>([]);
+
+  const onKeyDownEvent = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.keyCode !== 32) return;
+
+    if (value.toString().trim() === '') return;
+
+    const newTags = [...tags, {
+      id: Date.now() + value.toString(),
+      value: value.toString()
+    }];
+    
+    setTags(newTags);
+    setValue('');
+  }
+
+  const clearTag = (id: string) => {
+    const index = tags.findIndex(tag => tag.id === id);
+
+    const clearTag = [...tags];
+    clearTag.splice(index, 1)
+    setTags(clearTag);
+  }
+
   return (
-    <div className={styles.InputMulti + styleClassName}>
-      <InputTag>1</InputTag>
+    <label className={styles.InputMulti + styleClassName}>
+      {tags?.map((tag) => {
+        return (
+          <InputTag
+            key={tag.id}
+            onClick={() => clearTag(tag.id)}
+          >{tag.value}</InputTag>
+        )
+      })}
+
       <input
         className={styles.InputMulti__input}
+        onKeyDown={onKeyDownEvent}
+        value={value}
+        onChange={e => setValue(e.target.value)}
         type="text"
         {...rest}
       />
-    </div>
+    </label>
   );
 };
 
