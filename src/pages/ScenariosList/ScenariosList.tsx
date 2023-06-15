582a0b1668a630d021e7cd8f306b4977576851e2
Rostislav <rostik057@gmail.com>
2023-06-16 02:12:24 +0300

    !fix scenarios and segments pages

--- src/pages/ScenariosList/ScenariosList.tsx
+++ src/pages/ScenariosList/ScenariosList.tsx
@@ -5,8 +5,6 @@ import Select from '../../ui/Select/Select';
 import TableScenarios from '../../components/TableScenarios/TableScenarios';
 import Input from '../../ui/Input/Input';
 import SelectMulti from '../../ui/SelectMulti/SelectMulti';
-import TableInfoCard from '../../components/TableInfoCard/TableInfoCard';
-import TableInfoCardDonut from '../../components/TableInfoCardDonut/TableInfoCardDonut';
 
 interface ScenariosListProps { }
 
@@ -14,38 +12,6 @@ const ScenariosList: FC<ScenariosListProps> = () => (
   <div className={styles.ScenariosList}>
     <div className={styles.ScenariosList__top}>
       <Title>Сценарии</Title>
-      <div className={styles.ScenariosList__statistics}>
-        <TableInfoCard
-          title="Всего пользователей"
-          value={(<>
-            12,4 <span className="text-small">тыс</span>
-          </>)}
-        ></TableInfoCard>
-        <TableInfoCard
-          title="Электронная почта"
-          value={(<>
-            8,6 <span className="text-small">тыс</span>
-          </>)}
-        >
-          <TableInfoCardDonut value={65}></TableInfoCardDonut>
-        </TableInfoCard>
-        <TableInfoCard
-          title="Подтвердить подписку"
-          value={(<>
-            10,4 <span className="text-small">тыс</span>
-          </>)}
-        >
-          <TableInfoCardDonut value={95}></TableInfoCardDonut>
-        </TableInfoCard>
-        <TableInfoCard
-          title="7 дней активен"
-          value={(<>
-            3,4 <span className="text-small">тыс</span>
-          </>)}
-        >
-          <TableInfoCardDonut value={10}></TableInfoCardDonut>
-        </TableInfoCard>
-      </div >
       <div className={styles.ScenariosList__filter}>
         <Input
           placeholder='Поиск по наименованию'
--- src/pages/SegmentsList/SegmentsList.tsx
+++ src/pages/SegmentsList/SegmentsList.tsx
@@ -5,17 +5,59 @@ import Select from '../../ui/Select/Select';
 import TableSegments from '../../components/TableSegments/TableSegments';
 import Input from '../../ui/Input/Input';
 import SelectMulti from '../../ui/SelectMulti/SelectMulti';
+import Button from '../../ui/Button/Button';
+import TableInfoCard from '../../components/TableInfoCard/TableInfoCard';
+import TableInfoCardDonut from '../../components/TableInfoCardDonut/TableInfoCardDonut';
+import { Link } from 'react-router-dom';
 
 interface SegmentsListProps { }
 
 const SegmentsList: FC<SegmentsListProps> = () => (
   <div className={styles.SegmentsList}>
     <div className={styles.SegmentsList__top}>
-      <Title>
-        <div className={styles.SegmentsList__title}>
-          <div>Сценарии</div>
-        </div>
+      <Title className={styles.SegmentsList__title}>
+        <div>Сегменты</div>
+        <Link to="/segments/create">
+          <Button className='button_with_icon'>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M11 18V13H6V11H11V6H13V11H18V13H13V18H11Z" fill="white" />
+            </svg>
+            <span>Создать</span>
+          </Button>
+          </Link>
       </Title>
+      <div className={styles.SegmentsList__statistics}>
+        <TableInfoCard
+          title="Всего пользователей"
+          value={(<>
+            12,4 <span className="text-small">тыс</span>
+          </>)}
+        ></TableInfoCard>
+        <TableInfoCard
+          title="Электронная почта"
+          value={(<>
+            8,6 <span className="text-small">тыс</span>
+          </>)}
+        >
+          <TableInfoCardDonut value={65}></TableInfoCardDonut>
+        </TableInfoCard>
+        <TableInfoCard
+          title="Подтвердить подписку"
+          value={(<>
+            10,4 <span className="text-small">тыс</span>
+          </>)}
+        >
+          <TableInfoCardDonut value={95}></TableInfoCardDonut>
+        </TableInfoCard>
+        <TableInfoCard
+          title="7 дней активен"
+          value={(<>
+            3,4 <span className="text-small">тыс</span>
+          </>)}
+        >
+          <TableInfoCardDonut value={10}></TableInfoCardDonut>
+        </TableInfoCard>
+      </div>
       <div className={styles.SegmentsList__filter}>
         <Input
           placeholder='Поиск по наименованию'
