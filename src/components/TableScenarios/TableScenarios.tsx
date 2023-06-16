a3701c93a3050e2165a345a3957a65b3e336270a
Rostislav <rostik057@gmail.com>
2023-06-16 12:15:46 +0300

    +add data in table segments & table scenarios

--- src/components/TableScenarios/TableScenarios.tsx
+++ src/components/TableScenarios/TableScenarios.tsx
@@ -3,6 +3,7 @@ import styles from './TableScenarios.module.scss';
 import Table from '../Table/Table';
 import TableItem from '../Table/components/TableItem/TableItem';
 import TableScenariosItem from '../TableScenariosItem/TableScenariosItem';
+import { typeStatus } from '../../ui/Status/Status';
 
 interface TableScenariosProps { }
 
@@ -15,17 +16,71 @@ const TableScenarios: FC<TableScenariosProps> = () => {
     ''
   ];
 
+  const data = [
+    {
+      id: 1,
+      name: 'Наименование',
+      type: 'Заголовок',
+      statistic: '—',
+      status: 'active' as typeStatus,
+    },
+    {
+      id: 2,
+      name: 'Наименование',
+      type: 'Заголовок',
+      statistic: '—',
+      status: 'working' as typeStatus,
+    },
+    {
+      id: 3,
+      name: 'Наименование',
+      type: 'Заголовок',
+      statistic: '—',
+      status: 'active' as typeStatus,
+    },
+    {
+      id: 4,
+      name: 'Наименование',
+      type: 'Заголовок',
+      statistic: '—',
+      status: 'draft' as typeStatus,
+    },
+    {
+      id: 5,
+      name: 'Наименование',
+      type: 'Заголовок',
+      statistic: '—',
+      status: 'active' as typeStatus,
+    },
+    {
+      id: 6,
+      name: 'Наименование тест',
+      type: 'Заголовок',
+      statistic: '—',
+      status: 'pause' as typeStatus,
+    },
+    {
+      id: 7,
+      name: 'Наименование',
+      type: 'Заголовок',
+      statistic: '—',
+      status: 'active' as typeStatus,
+    }
+  ];
+
   return (
     <div className={styles.TableScenarios}>
       <Table
         titles={titles}
         styleGridTemplate={styles.TableScenarios__grid_template}
       >
-        <TableItem>
+        {data?.length && data.map(item => (<TableItem>
           <TableScenariosItem
+            data={item}
             styleGridTemplate={styles.TableScenarios__grid_template}
           ></TableScenariosItem>
-        </TableItem>
+        </TableItem>))
+        }
       </Table>
     </div>
   );
--- src/components/TableSegments/TableSegments.tsx
+++ src/components/TableSegments/TableSegments.tsx
@@ -18,17 +18,80 @@ const TableSegments: FC<TableSegmentsProps> = () => {
     ''
   ];
 
+  const data = [
+    {
+      id: 1,
+      name: 'Наименование',
+      date: '12 апр 2023',
+      count_companies: 456,
+      count_messages: 45,
+      count_tests: 5,
+    },
+    {
+      id: 2,
+      name: 'Наименование',
+      date: '12 апр 2023',
+      count_companies: 12,
+      count_messages: 0,
+      count_tests: 0,
+    },
+    {
+      id: 3,
+      name: 'Наименование',
+      date: '12 апр 2023',
+      count_companies: 456,
+      count_messages: 45,
+      count_tests: 5,
+    },
+    {
+      id: 4,
+      name: 'Наименование',
+      date: '12 апр 2023',
+      count_companies: 4312,
+      count_messages: 15,
+      count_tests: 55,
+    },
+    {
+      id: 5,
+      name: 'Наименование',
+      date: '12 апр 2023',
+      count_companies: 252,
+      count_messages: 15,
+      count_tests: 3,
+    },
+    {
+      id: 6,
+      name: 'Наименование тест',
+      count_companies: 456,
+      count_messages: 45,
+      count_tests: 5,
+    },
+    {
+      id: 7,
+      name: 'Наименование',
+      date: '12 апр 2023',
+      count_companies: 45,
+      count_messages: 4,
+      count_tests: 1,
+    }
+  ];
+
   return (
     <div className={styles.TableSegments}>
       <Table
         titles={titles}
         styleGridTemplate={styles.TableSegments__grid_template}
       >
-        <TableItem>
-          <TableSegmentsItem
-            styleGridTemplate={styles.TableSegments__grid_template}
-          ></TableSegmentsItem>
-        </TableItem>
+        {data?.length && data.map(item => (
+          <TableItem
+            key={item?.id}
+          >
+            <TableSegmentsItem
+              data={item}
+              styleGridTemplate={styles.TableSegments__grid_template}
+            ></TableSegmentsItem>
+          </TableItem>
+        ))}
       </Table>
     </div>
   );
